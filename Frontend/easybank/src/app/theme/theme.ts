b52e5be0eb004cbb8e654457c7234fbde063c8da
Mark <mdargie97@gmail.com>
2021-03-25 22:44:38 +0000

    Theme Service + Styling Change and Bug Fixes

--- Frontend/easybank/src/app/theme/theme.ts
+++ Frontend/easybank/src/app/theme/theme.ts
@@ -7,7 +7,13 @@ export const light: Theme = {
     name: "light",
     properties: {
       "--dashboard-main": "#0088c7",
-      "--dashboard-sidebar": "#0088c7"
+      "--dashboard-sidebar": "#0088c7",
+      "--dashboard-background":"#f1f1f1",
+      "--dashboard-text": "#070707",
+      "--dashboard-card": "#ffffff",
+      "--dashboard-tablehead":"#dddddd",
+      "--dashboard-header": "#ffffff",
+      "--dashboard-header-sep": "#d6d6d6f1"
     }
   };
 
@@ -15,7 +21,13 @@ export const light: Theme = {
     name: "dark",
     properties: {
         "--dashboard-main": "#0088c7",
-        "--dashboard-sidebar": "#1d2231"
+        "--dashboard-sidebar": "#1d2231",
+        "--dashboard-background":"#111111",
+        "--dashboard-text": "#e7e7e7",
+        "--dashboard-card": "#0f0f0f",
+        "--dashboard-tablehead":"#0c0c0c",
+        "--dashboard-header": "#0c0c0c",
+        "--dashboard-header-sep": "#000000f1"
     }
   };
 

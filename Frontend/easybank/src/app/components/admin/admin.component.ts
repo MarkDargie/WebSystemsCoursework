3c96e5c0025e6d14fb445cfb2430374566178592
Mark <mdargie97@gmail.com>
2021-03-26 07:10:16 +0000

    Admin Component Design Upate

--- Frontend/easybank/src/app/components/admin/admin.component.ts
+++ Frontend/easybank/src/app/components/admin/admin.component.ts
@@ -20,7 +20,7 @@ export class AdminComponent implements OnInit {
 
   transferpiechart = [];
 
-  groupedchart = [];
+  themechart = [];
 
   test: test;
 
@@ -37,7 +37,7 @@ export class AdminComponent implements OnInit {
           labels:["Secure Payment", "Express Payment"],
           datasets: [{
             label: "User Payment Methods",
-            backgroundColor: ["#3cba9f", "#ffcc00"],
+            backgroundColor: ["#0088c7", "#fd4b4b"],
             data: [this.test.securepayments, this.test.expresspayments]
           }]
         },
@@ -58,8 +58,8 @@ export class AdminComponent implements OnInit {
           labels:["Direct Download", "Sent Via Email"],
           datasets: [{
             label: "Transfer Statement Methods",
-            backgroundColor: ["#3cba9f", "#ffcc00"],
-            data: [4, 7]
+            backgroundColor: ["#0088c7", "#fd4b4b"],
+            data: [this.test.externalstatements, this.test.appstatements]
           }]
         },
         options: {
@@ -72,77 +72,31 @@ export class AdminComponent implements OnInit {
   
       });
 
-      //grouped bar chart
-      this.groupedchart = new Chart('groupedchart', {
-
-        type: 'bar',
+      this.themechart = new Chart('themechart', {
+        type:'pie',
         data: {
-          labels: ["Light", "Dark"],
-          datasets: [
-            {
-              label: "Population (millions)",
-              backgroundColor: ["#3e95cd", "#8e5ea2","#3cba9f","#e8c3b9","#c45850"],
-              data: [this.test.lighttheme, this.test.darktheme]
-            }
-          ]
+          labels:["Light Theme", "Dark Theme"],
+          datasets: [{
+            label: "Dashboard Theme Preference",
+            backgroundColor: ["#0088c7", "#fd4b4b"],
+            data: [this.test.lighttheme, this.test.darktheme]
+          }]
         },
         options: {
-          legend: { display: false },
           title: {
             display: true,
-            text: 'Predicted world population (millions) in 2050'
+            text: 'User Statement Method Preference'
           },
           maintainAspectRatio: false
         }
-
-
+  
       });
+
 
 
     });
 
-    // let testl: string[] = ['one','two', 'three'];
-    // let sets: number[] = [1, 2, 4];
-    // let sets2: number[] = [3, 5, 4];
-
-    // this.chart = new Chart('canvas', {
-    //   type:'line',
-    //   data: {
-    //     labels: testl,
-    //     datasets: [
-    //       {
-    //       data: sets,
-    //       borderColor: '#3cba9f',
-    //       fill: false
-    //     },
-    //     {
-    //       data: sets2,
-    //       borderColor: '#ffcc00',
-    //       fill: false
-    //     }
-      
-    //   ]
-    //   },
-    //   options: {
-    //     legend: {
-    //       display: false
-    //     },
-    //     scales: {
-    //       xAxes: [{
-    //         display: true
-    //       }],
-    //       yAxis: [{
-    //         display:true
-    //       }]
-    //     }
-    //   }
-    // })
-
   }
 
-  // ngAfterViewInit(){
-
-
-  // }
 
 }
